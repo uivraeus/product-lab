cb32d9440f8590562d2edfe85f191061b6e7dfb9
ulf <uivraeus@gmail.com>
2023-03-25 08:38:26 +0100

    Fix broken test

--- test_product.sh
+++ test_product.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
+expected_num_lines=5
 num_lines=$(cat product.txt | wc -l)
-if [ "$num_lines" == "4" ]; then
+if [ "$num_lines" == "$expected_num_lines" ]; then
   echo "Test pass"
 else
-  echo "Test failed, expected 4 lines, got ${num_lines}"
+  echo "Test failed, expected ${expected_num_lines} lines, got ${num_lines}"
   exit 1
 fi
